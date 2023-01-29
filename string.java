54267e0a5ce56e4378009a7c5c0f803810b8a016
numbercoder687 <98284348+numbercoder687@users.noreply.github.com>
2023-01-29 13:51:16 -0600

    java project

---
+++ string.java
@@ -0,0 +1,21 @@
+import java.io.*;   
+public class PrintDemo  
+{   
+public static void main(String[] args)   
+{   
+  
+int num = 122;   
+char ch = 'A';   
+String str = "Oracle";   
+double d = 190.98;   
+float f = 3.14f;   
+System.out.println();
+System.out.println(num);
+System.out.println(ch);   
+System.out.print(str+"\n");   
+System.out.print(d +"\n");   
+System.out.print(f+"\n");   
+System.out.printf("'%s' %n", "huh");  
+System.out.printf("'%S' %n", "alright");  
+}   
+}  
